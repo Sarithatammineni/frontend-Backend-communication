803c945382499b38c7c3d6136816e29c4c2f90a6
Sarithatammineni <sarithatammineni18@gmail.com>
2025-12-09 18:13:40 +0530

    first commit

---
+++ navbar.js
@@ -0,0 +1,9 @@
+export function navbar() {
+    return `
+        <nav style="background:#222; padding:15px; color:white;">
+            <a href="index.html" style="color:white; margin-right:20px;">Home</a>
+            <a href="signup.html" style="color:white; margin-right:20px;">Signup</a>
+            <a href="login.html" style="color:white;">Login</a>
+        </nav>
+    `;
+}
