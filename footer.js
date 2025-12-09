6505bbfd7084ab5d94e452aae52a322989cd9306
Sarithatammineni <sarithatammineni18@gmail.com>
2025-12-09 18:14:06 +0530

    second commit

---
+++ footer.js
@@ -0,0 +1,7 @@
+export function footer() {
+    return `
+        <footer style="background:#222; padding:15px; color:white; margin-top:20px; text-align:center;">
+            <p>© 2025 Chintu's App</p>
+        </footer>
+    `;
+}
